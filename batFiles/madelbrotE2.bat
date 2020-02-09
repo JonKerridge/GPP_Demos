ebb4caf72b3b38f5d9f9a021eecddb12a955ba6d
JonKe <jonkerridge@btinternet.com>
2020-02-09 10:39:41 +0000

    Pushing a number of small changes and revert to Java 11

---
+++ batFiles/madelbrotE2.bat
@@ -0,0 +1,8 @@
+call runDemo mandelbrot/scripts RunMandelbrotLineNoGUI   X-mandelbrot-Exp2 1 1024 5600 3200 0.000625
+call runDemo mandelbrot/scripts RunMandelbrotLineNoGUI   X-mandelbrot-Exp2 2 1024 5600 3200 0.000625
+call runDemo mandelbrot/scripts RunMandelbrotLineNoGUI   X-mandelbrot-Exp2 4 1024 5600 3200 0.000625
+call runDemo mandelbrot/scripts RunMandelbrotLineNoGUI   X-mandelbrot-Exp2 8 1024 5600 3200 0.000625
+call runDemo mandelbrot/scripts RunMandelbrotLineNoGUI   X-mandelbrot-Exp2 16 1024 5600 3200 0.000625
+call runDemo mandelbrot/scripts RunMandelbrotLineNoGUI   X-mandelbrot-Exp2 32 1024 5600 3200 0.000625
+call runDemo mandelbrot/scripts RunMandelbrotLineNoGUI   X-mandelbrot-Exp2 64 1024 5600 3200 0.000625
+call runDemo mandelbrot/scripts SeqMandelbrot   X-mandelbrot-Ex2  1024 5600 3200 0.000625
---
+++ src/main/groovy/GPP_Demos/QuickSortIntegers/RunGroupQuickSort.groovy
@@ -0,0 +1,104 @@
+package GPP_Demos.QuickSortIntegers
+
+import jcsp.lang.*
+import groovyJCSP.*
+ 
+import GPP_Demos.QuickSortIntegers.QSData as qsd
+import GPP_Demos.QuickSortIntegers.QSResult as qsr
+import GPP_Demos.QuickSortIntegers.QSWorker as qsw
+import GPP_Library.DataDetails
+import GPP_Library.LocalDetails
+import GPP_Library.ResultDetails
+import GPP_Library.connectors.reducers.N_WayMerge
+import GPP_Library.connectors.spreaders.OneDirectedList
+import GPP_Library.functionals.groups.ListThreePhaseWorkerList
+import GPP_Library.terminals.Collect
+import GPP_Library.terminals.Emit
+ 
+
+//usage runDemo QuickSort RunGroupQuickSort resultsFile workers instances
+ 
+ 
+int workers
+int instances
+if (args.size() == 0) {
+workers = 4
+instances = 5000
+} else {
+//    String folder = args[0] not used
+workers = Integer.parseInt(args[1])
+instances = Integer.parseInt(args[2])
+}
+print "QuickSort $workers, $instances, "
+ 
+System.gc()
+def startime = System.currentTimeMillis()
+ 
+def emitterDetails = new DataDetails(dName: qsd.getName(),
+dInitMethod: qsd.init,
+dInitData: [instances, workers],
+dCreateMethod: qsd.create)
+ 
+def resultDetails = new ResultDetails(rName: qsr.getName(),
+rInitMethod: qsr.init,
+rCollectMethod: qsr.collector,
+rFinaliseMethod: qsr.finalise)
+ 
+def workerDetails = new LocalDetails(lName: qsw.getName(),
+lInitMethod: qsw.init)
+ 
+
+//NETWORK
+
+def chan1 = Channel.one2one()
+def chan2 = Channel.one2oneArray(workers)
+def chan2OutList = new ChannelOutputList(chan2)
+def chan2InList = new ChannelInputList(chan2)
+def chan3 = Channel.one2oneArray(workers)
+def chan3OutList = new ChannelOutputList(chan3)
+def chan3InList = new ChannelInputList(chan3)
+def chan4 = Channel.one2one()
+
+def emitter = new Emit(
+    // input channel not required
+    output: chan1.out(),
+    eDetails: emitterDetails)
+ 
+def fan = new OneDirectedList(
+    input: chan1.in(),
+    outputList: chan2OutList,
+    indexProperty: "batch")
+ 
+def tpws = new ListThreePhaseWorkerList(
+    inputList: chan2InList,
+    outputList: chan3OutList,
+    inputMethod: qsw.inFunction,
+    workMethod: qsw.workFunction,
+    outFunction: qsw.outFunction,
+    lDetails: workerDetails,
+    workers: workers)
+ 
+ 
+def merge = new N_WayMerge(
+    inputList: chan3InList,
+    output: chan4.out(),
+    mergeChoice: qsd.mergeChoice,
+    inClassName: qsd.getName())
+ 
+def collector = new Collect(
+    input: chan4.in(),
+    // no output channel required
+    rDetails: resultDetails)
+
+PAR network = new PAR()
+ network = new PAR([emitter , fan , tpws , merge , collector ])
+ network.run()
+ network.removeAllProcesses()
+//END
+
+ 
+ 
+def endtime = System.currentTimeMillis()
+println " ${endtime - startime}"
+ 
+ 
